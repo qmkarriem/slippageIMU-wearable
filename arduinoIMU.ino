8aee064ecbacbc247ee26a5afc91744531f71908
Quran Karriem <quran@Qurans-MacBook-Pro.local>
2017-10-17 20:27:54 -0400

    photon-version

--- arduinoIMU.ino
+++ arduinoIMU.ino
@@ -15,12 +15,12 @@ SFE_LSM9DS1 library. It'll demo the following:
   variables section).
 * How to use the begin() function of the LSM9DS1 class.
 * How to read the gyroscope, accelerometer, and magnetometer
-  using the readGryo(), readAccel(), readMag() functions and 
+  using the readGryo(), readAccel(), readMag() functions and
   the gx, gy, gz, ax, ay, az, mx, my, and mz variables.
-* How to calculate actual acceleration, rotation speed, 
-  magnetic field strength using the calcAccel(), calcGyro() 
+* How to calculate actual acceleration, rotation speed,
+  magnetic field strength using the calcAccel(), calcGyro()
   and calcMag() functions.
-* How to use the data from the LSM9DS1 to calculate 
+* How to use the data from the LSM9DS1 to calculate
   orientation and heading.
 
 Hardware setup: This library supports communicating with the
@@ -31,12 +31,12 @@ to use I2C. The pin-out is as follows:
 	 SDA ---------- SDA (A4 on older 'Duinos')
 	 VDD ------------- 3.3V
 	 GND ------------- GND
-(CSG, CSXM, SDOG, and SDOXM should all be pulled high. 
+(CSG, CSXM, SDOG, and SDOXM should all be pulled high.
 Jumpers on the breakout board will do this for you.)
 
 The LSM9DS1 has a maximum voltage of 3.6V. Make sure you power it
-off the 3.3V rail! I2C pins are open-drain, so you'll be 
-(mostly) safe connecting the LSM9DS1's SCL and SDA pins 
+off the 3.3V rail! I2C pins are open-drain, so you'll be
+(mostly) safe connecting the LSM9DS1's SCL and SDA pins
 directly to the Arduino.
 
 Development environment specifics:
@@ -44,8 +44,8 @@ Development environment specifics:
 	Hardware Platform: SparkFun Redboard
 	LSM9DS1 Breakout Version: 1.0
 
-This code is beerware. If you see me (or any other SparkFun 
-employee) at the local, and you've found our code helpful, 
+This code is beerware. If you see me (or any other SparkFun
+employee) at the local, and you've found our code helpful,
 please buy us a round!
 
 Distributed as-is; no warranty is given.
@@ -78,17 +78,17 @@ LSM9DS1 imu;
 #define PRINT_SPEED 250 // 250 ms between prints
 static unsigned long lastPrint = 0; // Keep track of print time
 
-// Earth's magnetic field varies by location. Add or subtract 
-// a declination to get a more accurate heading. Calculate 
+// Earth's magnetic field varies by location. Add or subtract
+// a declination to get a more accurate heading. Calculate
 // your's here:
 // http://www.ngdc.noaa.gov/geomag-web/#declination
 #define DECLINATION -9.1 // Declination (degrees) in Durham, NC in 2017.
 
-void setup() 
+void setup()
 {
-  
+
   Serial.begin(115200);
-  
+
   // Before initializing the IMU, there are a few settings
   // we may need to adjust. Use the settings struct to set
   // the device's communication mode and addresses:
@@ -135,7 +135,7 @@ void loop()
     // mx, my, and mz variables with the most current data.
     imu.readMag();
   }
-  
+
   if ((lastPrint + PRINT_SPEED) < millis())
   {
     printGyro();  // Print "G: gx, gy, gz"
@@ -145,10 +145,10 @@ void loop()
     // Call print attitude. The LSM9DS1's mag x and y
     // axes are opposite to the accelerometer, so my, mx are
     // substituted for each other.
-    printAttitude(imu.ax, imu.ay, imu.az, 
+    printAttitude(imu.ax, imu.ay, imu.az,
                  -imu.my, -imu.mx, imu.mz);
     Serial.println();
-    
+
     lastPrint = millis(); // Update lastPrint time
   }
 }
@@ -178,7 +178,7 @@ void printGyro()
 }
 
 void printAccel()
-{  
+{
   // Now we can use the ax, ay, and az variables as we please.
   // Either print them as raw ADC values, or calculated in g's.
   Serial.print("A: ");
@@ -192,7 +192,7 @@ void printAccel()
   Serial.print(" ");
   Serial.println(imu.calcAccel(imu.az), 2);
   //Serial.println(" g");
-#elif defined PRINT_RAW 
+#elif defined PRINT_RAW
   Serial.print(imu.ax);
   Serial.print(", ");
   Serial.print(imu.ay);
@@ -203,7 +203,7 @@ void printAccel()
 }
 
 void printMag()
-{  
+{
   // Now we can use the mx, my, and mz variables as we please.
   // Either print them as raw ADC values, or calculated in Gauss.
   Serial.print("M: ");
@@ -235,25 +235,68 @@ void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
 {
   float roll = atan2(ay, az);
   float pitch = atan2(-ax, sqrt(ay * ay + az * az));
-  
+
   float heading;
   if (my == 0)
     heading = (mx < 0) ? PI : 0;
   else
     heading = atan2(mx, my);
-    
+
   heading -= DECLINATION * PI / 180;
-  
+
   if (heading > PI) heading -= (2 * PI);
   else if (heading < -PI) heading += (2 * PI);
   else if (heading < 0) heading += 2 * PI;
-  
+
   // Convert everything from radians to degrees:
   heading *= 180.0 / PI;
   pitch *= 180.0 / PI;
   roll  *= 180.0 / PI;
-  
+
   Serial.print("Pitch: "); Serial.println(pitch, 2);
   Serial.print("Roll: "); Serial.println(roll, 2);
   Serial.print("Heading: "); Serial.println(heading, 2);
 }
+
+/*
+// EXAMPLE USAGE
+
+// UDP Port used for two way communication
+unsigned int localPort = 8888;
+
+// An UDP instance to let us send and receive packets over UDP
+UDP Udp;
+
+void setup() {
+  // start the UDP
+  Udp.begin(localPort);
+
+  // Print your device IP Address via serial
+  Serial.begin(9600);
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // Check if data has been received
+  if (Udp.parsePacket() > 0) {
+
+    // Read first char of data received
+    char c = Udp.read();
+
+    // Ignore other chars
+    while(Udp.available())
+      Udp.read();
+
+    // Store sender ip and port
+    IPAddress ipAddress = Udp.remoteIP();
+    int port = Udp.remotePort();
+
+    // Echo back data to sender
+    Udp.beginPacket(ipAddress, 8888);
+    Udp.write(c);
+    Serial.print(ipAddress); Serial.print(", "); Serial.print(port); Serial.print(", "); Serial.println(c);
+    Udp.endPacket();
+  }
+
+}
+*/
